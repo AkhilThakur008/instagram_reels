175cf6cf4d9a45e89ce1dc09be3b5c1aeb91f3f6
Akhil Thakur <takhil498@gmail.com>
2025-11-25 18:15:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,146 @@
+const reels=[
+  {
+    "username": "travel_with_amy",
+    "likeCount": 12043,
+    "isLiked": false,
+    "commentCount": 342,
+    "caption": "Sunsets in Bali just hit different 🌅✨",
+    "video": "videos/1.mp4",
+    "userProfile": "https://images.unsplash.com/photo-1761839257144-297ce252742e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "shareCount": 210,
+    "isFollowed": true
+  },
+  {
+    "username": "fitness_jay",
+    "likeCount": 8934,
+    "isLiked": true,
+    "commentCount": 187,
+    "caption": "Quick morning workout to kickstart the day 💪🔥",
+    "video": "videos/2.mp4",
+    "userProfile": "https://images.unsplash.com/photo-1764017884266-b53a65cf0044?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "shareCount": 98,
+    "isFollowed": false
+  },
+  {
+    "username": "foodie_mia",
+    "likeCount": 15022,
+    "isLiked": false,
+    "commentCount": 420,
+    "caption": "Homemade ramen is unbeatable 🍜❤️",
+    "video": "videos/3.mp4",
+    "userProfile": "https://images.unsplash.com/photo-1763713382836-e2263bff42b3?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "shareCount": 330,
+    "isFollowed": true
+  },
+  {
+    "username": "coder_luke",
+    "likeCount": 4221,
+    "isLiked": false,
+    "commentCount": 64,
+    "caption": "New JavaScript tricks I learned today 👨‍💻✨",
+    "video": "videos/4.mp4",
+    "userProfile": "https://images.unsplash.com/photo-1762764919450-560fd6515192?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "shareCount": 55,
+    "isFollowed": false
+  },
+  {
+    "username": "streetstyle_zoe",
+    "likeCount": 17892,
+    "isLiked": true,
+    "commentCount": 509,
+    "caption": "Today’s outfit: comfy but stylish 👟🖤",
+    "video": "videos/5.mp4",
+    "userProfile": "https://images.unsplash.com/photo-1763598363324-cd072a67dc61?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "shareCount": 412,
+    "isFollowed": true
+  },
+  {
+    "username": "petlover_sam",
+    "likeCount": 13200,
+    "isLiked": false,
+    "commentCount": 380,
+    "caption": "My dog learned a new trick today! 🐶🎉",
+    "video": "videos/6.mp4",
+    "userProfile": "https://plus.unsplash.com/premium_photo-1763265293425-f7ad17012b13?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "shareCount": 260,
+    "isFollowed": false
+  },
+  {
+    "username": "music_by_kai",
+    "likeCount": 20344,
+    "isLiked": true,
+    "commentCount": 720,
+    "caption": "New guitar riff I’ve been working on 🎸🔥",
+    "video": "videos/7.mp4",
+    "userProfile": "https://images.unsplash.com/photo-1762325658226-3a8bc64a6fd8?q=80&w=1979&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "shareCount": 520,
+    "isFollowed": true
+  },
+  {
+    "username": "artsy_elena",
+    "likeCount": 9577,
+    "isLiked": false,
+    "commentCount": 243,
+    "caption": "Finished a new sketch today ✏️✨",
+    "video": "videos/8.mp4",
+    "userProfile": "https://images.unsplash.com/photo-1762799585109-fd61a1aef92a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "shareCount": 140,
+    "isFollowed": false
+  },
+  {
+    "username": "tech_reviews_max",
+    "likeCount": 11209,
+    "isLiked": true,
+    "commentCount": 315,
+    "caption": "Testing out the new smartphone—camera is insane 📱🔥",
+    "video": "videos/9.mp4",
+    "userProfile": "https://images.unsplash.com/photo-1762795116942-1faf2ae6cf84?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "shareCount": 289,
+    "isFollowed": true
+  },
+  {
+    "username": "nature_nora",
+    "likeCount": 14088,
+    "isLiked": false,
+    "commentCount": 402,
+    "caption": "Nature walks clear the mind 🍃🌿",
+    "video": "videos/10.mp4",
+    "userProfile": "https://images.unsplash.com/photo-1762391965624-279023a2481d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "shareCount": 310,
+    "isFollowed": false
+  }
+]
+
+var sum ='';
+reels.forEach(function(elem){
+    sum =sum+ ` <div id="card">
+          <video autoplay loop muted playsinline src=${elem.video}></video>
+          <div class="info">
+            <div class="user-profile">
+                <img src=${elem.userProfile} alt="w" />
+                <h3>${elem.username}</h3>
+                <button>${elem.isFollowed ? "Following" : "Follow"}</button>
+            </div>
+            <p>
+              ${elem.caption}
+            </p>
+             <div>
+             </div>
+          </div>
+          <div class="actions">
+            <div> ${elem.isLiked ? `<i id="love" class="ri-heart-3-fill"></i>` : `<i id="love-2" class="ri-heart-3-line"></i>`}
+            <h4>${elem.likeCount}</h4>
+            </div>
+            <div><i class="ri-chat-1-line"></i>
+            <h4>${elem.commentCount}</h4>
+            </div>
+            <div>
+            <i class="ri-share-forward-line"></i>
+            <h4>${elem.shareCount}</h4>
+            </div>
+           
+          </div>
+        </div>`
+})
+
+document.querySelector("section").innerHTML=sum;
